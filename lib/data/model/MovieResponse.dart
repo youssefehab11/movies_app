16c91a0ff15b277a982cc770586aa0e17b3807cc
youssef <ucifehab11@gmail.com>
2024-12-10 03:26:36 +0200

    finished explore tab

---
+++ lib/data/model/MovieResponse.dart
@@ -0,0 +1,37 @@
+import 'Results.dart';
+
+class MovieResponse {
+  MovieResponse({
+      this.page, 
+      this.results, 
+      this.totalPages, 
+      this.totalResults,});
+
+  MovieResponse.fromJson(dynamic json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = [];
+      json['results'].forEach((v) {
+        results?.add(Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+  num? page;
+  List<Results>? results;
+  num? totalPages;
+  num? totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = page;
+    if (results != null) {
+      map['results'] = results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = totalPages;
+    map['total_results'] = totalResults;
+    return map;
+  }
+
+}
---
+++ lib/data/model/genre.dart
@@ -0,0 +1,27 @@
+import 'Genres.dart';
+
+class Genre {
+
+  List<Genres>? genres;
+
+  Genre({
+      this.genres,});
+
+  Genre.fromJson(dynamic json) {
+    if (json['genres'] != null) {
+      genres = [];
+      json['genres'].forEach((v) {
+        genres?.add(Genres.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (genres != null) {
+      map['genres'] = genres?.map((v) => v.toJson()).toList();
+    }
+    return map;
+  }
+
+}
