7a46ceeb57464111b72055337fe6c6f3d7ad3f2b
youssef <ucifehab11@gmail.com>
2024-12-10 05:20:56 +0200

    refactor end point

--- lib/core/api_manager/end_points.dart
+++ lib/core/api_manager/end_points.dart
@@ -1,4 +1,4 @@
 class EndPoints {
-  static const String genres = '3/genre/movie/list.';
+  static const String genres = '3/genre/movie/list';
   static const String exploreMovies = '3/discover/movie';
 }
