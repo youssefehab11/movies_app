508d87f3f2bbd3feaaa89b50999e176d5d309031
youssef <ucifehab11@gmail.com>
2024-12-10 05:20:14 +0200

    handling errors

---
+++ lib/data/model/ErrorResponse.dart
@@ -0,0 +1,24 @@
+class ErrorResponse {
+  ErrorResponse({
+      this.success, 
+      this.statusCode, 
+      this.statusMessage,});
+
+  ErrorResponse.fromJson(dynamic json) {
+    success = json['success'];
+    statusCode = json['status_code'];
+    statusMessage = json['status_message'];
+  }
+  bool? success;
+  num? statusCode;
+  String? statusMessage;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['success'] = success;
+    map['status_code'] = statusCode;
+    map['status_message'] = statusMessage;
+    return map;
+  }
+
+}
